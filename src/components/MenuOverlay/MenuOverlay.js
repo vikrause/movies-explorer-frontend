1211468c69a3b00575a64dde4d652f686542f3da
vikrause <vikrause@gmail.com>
2024-09-23 19:20:09 +0300

    gh-pages

--- src/components/MenuOverlay/MenuOverlay.js
+++
@@ -1,9 +0,0 @@
-import "./MenuOverlay.css"
-
-export default function MenuOverlay({ isOpen, children }) {
-    return (
-        <div className={`menu-overlay ${isOpen ? `menu-overlay_active` : ""}`}>
-            {children}
-        </div>
-    )
-}
